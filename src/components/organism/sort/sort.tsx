e0c1164cfe3202dd5b42a09958914ba3f0bb375a
taras131 <mossnabitkana@gmail.com>
2022-09-12 19:49:54 +0300

    refactor interface and order selector

--- src/components/organism/sort/sort.tsx
+++ src/components/organism/sort/sort.tsx
@@ -2,19 +2,16 @@ import React, {FC} from "react";
 import SortItem from "../../molecules/sort-item/sort-item";
 import SetLimit from "../../molecules/set-limit/set-limit";
 import styles from "./sort.module.scss";
-import {useAppSelector} from "../../../hooks/redux";
-import {getOrder} from "../../../store/link/selector";
 import {sortVariants} from "../../../config/constants";
 
 const Sort: FC = React.memo(() => {
-    const order = useAppSelector(state => getOrder(state));
     return (
         <div className={styles.wrapper}>
             <SetLimit/>
             <div className={styles.sort_container}>
-                <SortItem name={sortVariants.short} value={order.short}/>
-                <SortItem name={sortVariants.counter} value={order.counter}/>
-                <SortItem name={sortVariants.target} value={order.target}/>
+                <SortItem name={sortVariants.short}/>
+                <SortItem name={sortVariants.counter}/>
+                <SortItem name={sortVariants.target}/>
             </div>
         </div>
     );
